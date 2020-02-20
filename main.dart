cdcece337f1aa0d2e57b6de1827a6971c3ec4377
Shaunelle T <29800719+nelly2daworld@users.noreply.github.com>
2020-02-20 17:31:45 -0500

    SkeletonDashboard
    This is a simple dashboard with 2 dynamic boxes. 
    -> Color Scheme will be improved in future

--- main.dart
+++ main.dart
@@ -26,7 +26,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,32 +36,71 @@ class _MyHomePageState extends State<MyHomePage> {
       body: GridView(
         padding: EdgeInsets.all(20.0),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
+          crossAxisCount: MediaQuery.of(context).size.width >= 1100
+              ? 3
+              : MediaQuery.of(context).size.width >= 600 ? 2 : 1,
         ),
         children: <Widget>[
           Card(
             color: Colors.tealAccent[200],
             child: Center(
-            child: Text("Student Information", 
-            style:TextStyle(
-              fontSize: 20.0 ,
-              color: Colors.white,
+              child: Text(
+                "Student Information",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
           ),
           Card(
             color: Colors.black,
             child: Center(
-            child: Text("Student Information", 
-            style:TextStyle(
-              fontSize: 20.0 ,
-              color: Colors.white,
+              child: Text(
+                "Student Information",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
-          ),
         ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.lightBlue[100],
+                    Colors.cyan[50],
+                  ],
+                ),
+              ),
+              currentAccountPicture: FlutterLogo(
+                colors: Colors.blue,
+              ),
+              accountName: Text(
+                "First Name  Last Name",
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 1.0,
+                  fontSize: 20.0,
+                ),
+              ),
+              accountEmail: Text(
+                "student@example.com",
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 1.0,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
