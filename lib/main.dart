a509a09e7ecb5ff7b450908ba91adc5b6fec4910
Shaunelle T <29800719+nelly2daworld@users.noreply.github.com>
2020-02-21 23:44:49 -0500

    Starter Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
-  
+import 'package:last_stop/layout.dart';
 import 'package:flutter/material.dart';
-import 'package:firststop/pages/root_page.dart';
-import 'package:firststop/utils/auth.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
+  static const int _whitePrimaryValue = 0xFFFFFFFF;
+  static const MaterialColor white = MaterialColor(
+    _whitePrimaryValue,
+    <int, Color>{
+      50: Color(0xFFFFFFFF),
+      100: Color(0xFFFFFFFF),
+      200: Color(0xFFFFFFFF),
+      300: Color(0xFFFFFFFF),
+      400: Color(0xFFFFFFFF),
+      500: Color(_whitePrimaryValue),
+      600: Color(0xFFFFFFFF),
+      700: Color(0xFFFFFFFF),
+      800: Color(0xFFFFFFFF),
+      900: Color(0xFFFFFFFF),
+    },
+  );
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColorDark: Colors.white,
-      ),
-      home: new RootPage(auth: new FirebaseAuth()),
+      title: 'Dashboard',
+      theme: ThemeData(primarySwatch: white),
+      home: MyHomePage(),
     );
   }
 }
